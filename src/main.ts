0076c76955169d2ddac01d67f115c1eebae8cb67
Baver <bavertorunx@gmail.com>
2024-08-03 15:59:39 +0300

    feat: Addedd SwaggerUI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('To-Do Application Backend API')
+    .setDescription(
+      'This project provides a backend API for a To-Do (task management) application. The project includes features such as user management and authentication, going beyond basic CRUD operations.',
+    )
+    .setVersion('1.0')
+    .addTag('ToDoAPI')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
   await app.listen(3000);
+  console.log(`Applicaton running on ${await app.getUrl()}`);
 }
 bootstrap();
